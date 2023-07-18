665453b86348ed3182d868c4e07cc3593c5ecd04
jcsii98 <jose.c.saribong@gmail.com>
2023-07-18 22:22:40 +0800

    build setup and CORS trial

--- src/components/BodyButton.jsx
+++ src/components/BodyButton.jsx
@@ -1,7 +1,27 @@
 export default function BodyButton() {
+  const handleClick = () => {
+    fetch("https://journal-api-cxui.onrender.com/auth/signin", {
+      method: "POST", // Adjust the HTTP method as per your API endpoint requirements
+      headers: {
+        "Content-Type": "application/json",
+        email: "jose3@email.com",
+        password: "password", // Adjust the headers as needed
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data here
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        console.error(error);
+      });
+  };
+
   return (
     <>
-      <button className="body-btn btn-link">
+      <button className="body-btn btn-link" onClick={handleClick}>
         <h2 className="font-color-primary">For Today</h2>
       </button>
     </>
