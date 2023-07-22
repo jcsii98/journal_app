92bd51923a81a28b6090eb53ac4f2d8189d2b82d
jcsii98 <jose.c.saribong@gmail.com>
2023-07-23 02:51:27 +0800

    api url for deployment

--- src/pages/MainDash.jsx
+++ src/pages/MainDash.jsx
@@ -310,7 +310,7 @@ export default function MainDash(props) {
   };
 
   const handleEditTaskSubmit = (formData) => {
-    const url = `http://127.0.0.1:3000/categories/${catId}/tasks/${editTaskId}`;
+    const url = `https://journal-app-t8n9.onrender.com/categories/${catId}/tasks/${editTaskId}`;
     const method = "PUT";
 
     fetch(url, {
