51d0143183069425a516a2e4e8317f73ebdb37d0
jcsii98 <jose.c.saribong@gmail.com>
2023-08-04 20:00:32 +0800

    build files and apiurl

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import AuthPage from "./pages/authPage.jsx";
 import App from "./App.jsx";
 import "./styles.css";
 import "./index.css";
